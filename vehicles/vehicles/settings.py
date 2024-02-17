0224fae453d15695282613418fd6143891541485
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2024-02-17 12:54:46 +0100

    Update : allow 127.0.0.1

--- vehicles/vehicles/settings.py
+++ vehicles/vehicles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8vtrf8wdjmk47u7p*%alh#0a2_js6ppafl8r9#%$7!6=_e2ms=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
 
 
 # Application definition
