0c8c864f8ac71df60bba24408af900cdfdbfb4ca
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2024-02-25 15:47:35 +0100

    Update: just asign users to customers

--- bookcar_project/customer_app/serializers.py
+++ bookcar_project/customer_app/serializers.py
@@ -5,7 +5,7 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'name', 'email', 'phone_number', 'created_at']
+        fields = ['id', 'name', 'email', 'phone_number', 'created_at', 'user']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
